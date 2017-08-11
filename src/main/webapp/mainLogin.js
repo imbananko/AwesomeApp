8cc86715d43d5bd5aab0ee6fe3f9a25caf507ba0
ShokiRichard <Graduate@LO45863>
2017-08-11 16:28:50 +0100

    test connection, login , R4 basic features. but not good

--- src/main/webapp/mainLogin.js
+++ src/main/webapp/mainLogin.js
@@ -18,7 +18,7 @@ function verifyUser() {
     var userId = document.getElementById("f_userid");
     var password = document.getElementById("f_password");
     var url = "login.jsp?id=" + escape(userId.value) + "&pwd=" + escape(password.value);
-    //var url = 'https://learnwebcode.github.io/json-example/animals-1.json';
+
 
 
     $.ajax({url: url, dataType: "text", success: function (result)
@@ -31,33 +31,23 @@ function verifyUser() {
 
                 data.innerHTML += "invalid";
             }
-            //setMessageUsingDOM( result );
+
         }});
-    /*
-     ajaxRequest.open('GET',url);
-     ajaxRequest.onload = function(){
-     var dealRecord = ajaxRequest.responseText;
-     //var dealRecord = JSON.parse(ajaxRequest.responseText);
-     };
-     ajaxRequest.send();
-     //document.write("test");
-     //$('dealMessage').html("<div>ERROR IN PAGE</div>");
-     */
-    //renderData(dealRecord);
-    //document.write("<h2>" + dealRecord + "</h2>" );
 
 }
 
 function renderData() {
-    //$('dealMessage').html("<div>ERROR IN PAGE</div>");
-    //document.write("test");
-    //var dealData = JSON.parse(message);
+
     var data = document.getElementById("showData");
     data.innerHTML += "invalid";
 
 }
 
 function load_jsp() {
-    //document.write();
-    document.getElementById("showData").innerHTML = '<object type="text/html" data="indexR4.jsp" ></object>';
+    document.write(document.getElementById("showData").innerHTML = '<object type="text/html" data="indexR4.jsp" ></object>');
+}
+
+
+function testConnection(){
+    document.getElementById("showConn").innerHTML = '<object type="text/html" data="TestConnection" ></object>';
 }
